0629ffea73d96d3f7f2def81bd8a725d2653f532
liushuai <shuai.liu@xiaoyangedu.com>
2022-01-07 21:41:39 +0800

    【commit 上传案例】 Signed-off-by: liushuai <shuai.liu@xiaoyangedu.com>

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"errors"
+	"github.com/gin-gonic/gin"
+	valid "github.com/lshsuper/gin-validator/validator"
+	"net/http"
+)
+
+type Request struct {
+	valid.BaseResult
+	Phone   string  `json:"phone" binding:"phone"`
+	UserID  int     `json:"userId" binding:"gt=0"`
+	Content string  `json:"content" binding:"must"`
+	Age     int     `json:"age" binding:"gt=20"`
+	Money   float64 `json:"money" binding:"gt=35.6"`
+}
+
+//Check 重写check,做一些定制操作
+func (r *Request) Check(validTag, tag, param string) error {
+
+	//这里做重写，可以得到定制化的提示信息
+	if tag == "money" {
+		return errors.New("这数字不对路子")
+	}
+
+	return r.BaseResult.Check(validTag, tag, param)
+
+}
+
+func main() {
+
+	r := gin.Default()
+
+	//全局注册验证器
+	valid.Register()
+
+	r.POST("/post", func(context *gin.Context) {
+		var req Request
+		if err := valid.Form(context).CheckJSON(&req); err != nil {
+			context.JSON(http.StatusOK, err.Error())
+			return
+		}
+
+		context.JSON(http.StatusOK, "ok")
+
+	})
+
+	r.Run(":10086")
+
+}
